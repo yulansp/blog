23cca397670859f0e2f3d512377f9a6b9b71cdb9
yulansp <1301481108@qq.com>
2020-03-04 21:59:17 +0800

    formal version

--- src/yulan_blog/www/app.py
+++
@@ -1,3 +0,0 @@
-from handlers import main
-if __name__ == '__main__':
-    main()
--- src/yulan_blog/www/shici_change.py
+++
@@ -1,7 +0,0 @@
-import os,random
-oldfiledir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'shici')
-dirlist = os.listdir(oldfiledir)
-old_file_path = os.path.join(oldfiledir, dirlist[random.randint(0, len(dirlist) - 1)])
-with open(old_file_path,'rb') as fold:
-    with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates', 'shici.html'), 'wb') as newf:
-        newf.write(fold.read())
